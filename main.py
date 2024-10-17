3c64d3361a419a1593b6df23cef56740eff6663b
Esmeail Mashhadi <codealchemy.esi@gmail.com>
2024-10-17 10:28:47 +0330

    final refactoring done

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
 import streamlit as st
-import pandas as pd 
-import plotly.express as px 
 from components.weather_Time_taken.weather_effect_tab import weather_effects_tab
 from components.weather_Time_taken.Traffic_effect_tab import Traffic_effect_tab
 from components.weather_Time_taken.Age_effect_tab import Age_effect_tab
 from components.weather_Time_taken.Show_map import show_map
+from streamlit_navigation_bar import st_navbar
 
 
-st.header('Exploring Food Delivery Insights through Interactive Data Visualizations')
 
- 
-def display_map(location_data):
-    pass
+page = st_navbar(["Data Analytics Overview", "Geospatial Map", "AI Predictions", "Source Code & Contact"])
 
 
-st.sidebar.title('Options')  
+if page == 'Data Analytics Overview' : 
+    st.subheader('Data Analytics Overview') 
 
-
-if st.sidebar.toggle('Data Interaction Overview' , open):
-    st.write('relation menu')
-    tab1, tab2, tab3 = st.tabs(["Weather Effects", "Traffic Impact", "Age Correlations"])
-
+    tab1, tab2, tab3 = st.tabs(["Weather Effects", "Traffic Impact", "Age Correlations"])    
     with tab1: 
         weather_effects_tab()
 
@@ -30,9 +23,38 @@ if st.sidebar.toggle('Data Interaction Overview' , open):
     with tab3:
        Age_effect_tab()
 
-if st.sidebar.toggle('show map') :
-         show_map()
+
+if  page == 'Geospatial Map': 
+    show_map() 
       
+if page =="AI Predictions": 
+    st.info('comming soon')
+
+import streamlit as st
+
+if page == 'Source Code & Contact':
+    st.subheader("Source Code & Contact Information")
+
+    st.write("Here are the relevant links to my projects and profiles:")
+
+    col1, col2, col3, col4 = st.columns(4)
+
+    with col1:
+        if st.button('GitHub Profile'):
+            st.write('[My GitHub Profile](https://github.com/Esmaeil-mashhadi)')
+
+    with col2:
+        if st.button('Jupyter Notebook'):
+            st.write('[Data Cleaning Source](https://github.com/Esmaeil-Mashhadi/streamlit_dataVisualization/blob/main/dataCleaning.ipynb)')
+
+    with col3:
+        if st.button('Streamlit Source'):
+            st.write('[Streamlit Source Code](https://github.com/Esmaeil-Mashhadi/streamlit_dataVisualization/blob/main/main.py)')
+
+    with col4: 
+        if st.button('phone & email'): 
+            st.write('+989136038055')
+            st.write('codealchemy.esi@gmail.com')
 
 
 
